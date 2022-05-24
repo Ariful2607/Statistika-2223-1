504b52d29b1f9de52760ce35d09d5710490b6dc4
Muhammad Ariful Furqon, S.Pd., M.Kom <82745904+Ariful2607@users.noreply.github.com>
2022-05-24 13:52:33 +0700

    Update minggu 11

--- 11 Distribusi Multinomial, Normal, dan Kai Kuadrat/Distribusi Multinomial, Normal, dan Kai Kuadrat.R
+++ 11 Distribusi Multinomial, Normal, dan Kai Kuadrat/Distribusi Multinomial, Normal, dan Kai Kuadrat.R
@@ -1,33 +1,26 @@
 
-                    ###   Distribusi Multinomial   ###
+                                                                ###   Distribusi Multinomial   ###
 
 
-# The multinomial distribution describes the probability of obtaining a 
-# specific number of counts for k different outcomes, when each outcome 
+# The multinomial distribution describes the probability of obtaining a specific number of counts for k different outcomes, when each outcome 
 # has a fixed probability of occurring. independen
 
-# If a random variable X follows a multinomial distribution, then the 
-# probability that outcome 1 occurs exactly x1 times, outcome 2 occurs exactly 
+# If a random variable X follows a multinomial distribution, then the probability that outcome 1 occurs exactly x1 times, outcome 2 occurs exactly 
 # x2 times, etc.
 
-# To calculate a multinomial probability in R we can use the dmultinom() 
-# function, which uses the following syntax:
+# To calculate a multinomial probability in R we can use the dmultinom() function, which uses the following syntax:
   
 dmultinom(x=c(1, 6, 8), prob=c(.4, .5, .1))
 
 # where:
 # x    : A vector that represents the frequency of each outcome
-# prob : A vector that represents the probability of each outcome 
-#        (the sum must be 1)
+# prob : A vector that represents the probability of each outcome (the sum must be 1)
 
 # Example
 
-# Proses pembuatan pensil dalam sebuah pabrik melibatkan banyak buruh dan 
-# dilakukan berulang-ulang. Pada suatu pemeriksaan terakhir terlihat bahwa 85% 
-# produk adalah ”baik”, 10% ”tidak baik tetapi masih bisa diperbaiki” dan 5% 
-# ”rusak dan harus dibuang”. Jika sebuah sampel acak dengan 20 unit dipilih, 
-# berapa peluang jumlah unit ”baik” sebanyak 18, unit ”tidak baik tetapi bisa 
-# diperbaiki” sebanyak 2 dan unit ”rusak” tidak ada? (Jawaban : 0,102)
+# Proses pembuatan pensil dalam sebuah pabrik melibatkan banyak buruh dan dilakukan berulang-ulang. Pada suatu pemeriksaan terakhir terlihat bahwa 85% 
+# produk adalah ”baik”, 10% ”tidak baik tetapi masih bisa diperbaiki” dan 5% ”rusak dan harus dibuang”. Jika sebuah sampel acak dengan 20 unit dipilih, 
+# berapa peluang jumlah unit ”baik” sebanyak 18, unit ”tidak baik tetapi bisa diperbaiki” sebanyak 2 dan unit ”rusak” tidak ada? (Jawaban : 0,102)
 
 p_x1 = 0.85
 p_x2 = 0.1
@@ -40,13 +33,11 @@ x3 = 0
 dmultinom(x=c(x1, x2, x3), prob=c(p_x1, p_x2, p_x3))
 
 
-                      ###   Distribusi Normal   ###
+                                                                ###   Distribusi Normal   ###
 
 
-# Normal Distribution is a probability function used in statistics that tells 
-# about how the data values are distributed. It is the most important 
-# probability distribution function used in statistics because of its advantages 
-# in real case scenarios. For example, the height of the population, shoe size, 
+# Normal Distribution is a probability function used in statistics that tells about how the data values are distributed. It is the most important 
+# probability distribution function used in statistics because of its advantages in real case scenarios. For example, the height of the population, shoe size, 
 # IQ level, rolling a dice, and many more.
 
 # In R, there are 4 built-in functions to generate normal distribution:
@@ -72,10 +63,8 @@ dev.off()
 
 # Example
 
-# Sebuah laptop dengan merk X, mengklaim bahwa lamanya waktu ketahanan daya 
-# baterai didistribusikan secara normal dengan rata-rata adalah 5 jam dan 
-# standar deviasi 1,5 jam. Bejo membeli laptop tersebut dan ingin mengetahui 
-# ketahanan daya baterai laptop tersebut untuk penggunaan normal. Bantulah Bejo 
+# Sebuah laptop dengan merk X, mengklaim bahwa lamanya waktu ketahanan daya baterai didistribusikan secara normal dengan rata-rata adalah 5 jam dan 
+# standar deviasi 1,5 jam. Bejo membeli laptop tersebut dan ingin mengetahui ketahanan daya baterai laptop tersebut untuk penggunaan normal. Bantulah Bejo 
 # untuk menghitung probabilitas laptop tersebut dapat bertahan 6 jam!
 
 rata2 <- 5
@@ -93,13 +82,11 @@ prob <- dnorm(x,rata2,std)
 prob
 
 
-                  ###   Distribusi Kai Kuadrat   ###
+                                                              ###   Distribusi Kai Kuadrat   ###
 
 
-# The chi-squared distribution with df degrees of freedom is the distribution 
-# computed over the sums of the squares of df independent standard normal 
-# random variables. This distribution is used for the categorical analysis of 
-# the data.
+# The chi-squared distribution with df degrees of freedom is the distribution computed over the sums of the squares of df independent standard normal 
+# random variables. This distribution is used for the categorical analysis of the data.
 
 # In R, there are 4 built-in functions to generate chi-squared distribution:
 
@@ -109,7 +96,7 @@ prob
 # rchisq() -> random numbers from the chi-square distribution
 
 # defining degrees of freedom
-df = 5 #tingkat kesalahan yang dapat ditolerir maksimal 5%
+df = 5 # tingkat kesalahan yang dapat ditolerir maksimal 5%
 
 # computing values of 50k random values with 5 degrees of freedom
 x <- rchisq(50000, df = 5)
